2165021afcbcf514c10a5bbf9a998b80c0aea29f
gminos <gjosebelarmino@gmail.com>
2025-11-21 16:35:49 -0500

    Agregar configuración de URL y vista para el modelo Task

--- task/views.py
+++ task/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    @action(detail=True, methods=['post'])
+    def toggle_complete(self, request, pk=None):
+        task = self.get_object()
+        task.is_completed = not task.is_completed
+        task.save()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
