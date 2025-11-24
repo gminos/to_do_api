94f3f04f02a1bc08f8ba5fc7462528230a167d4d
gminos <gjosebelarmino@gmail.com>
2025-11-24 09:15:11 -0500

    fix: Eliminar las restricciones innecesarias de blank y null en los campos title y priority del modelo Task

--- task/models.py
+++ task/models.py
@@ -4,10 +4,10 @@ from django.db import models
 PRIORITIES = [(3, "Low"),(2, "Medium"),(1, "High")]
 
 class Task(models.Model):
-    title = models.CharField(max_length=200, blank=False)
+    title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     is_complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     due_date = models.DateField(null=True, blank=True)
-    priority = models.IntegerField(blank=False, null=False, choices=PRIORITIES, default=3)
+    priority = models.IntegerField(choices=PRIORITIES, default=3)
