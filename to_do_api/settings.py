6532b2ad6078d2f16a3f67baa8dc61daa9bc9749
gminos <gjosebelarmino@gmail.com>
2025-11-25 17:37:18 -0500

    chore: Eliminar SessionAuthentication y añadir JSONRenderer como renderizador predeterminado

--- to_do_api/settings.py
+++ to_do_api/settings.py
@@ -53,14 +53,17 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
 
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer'
+    ]
 
 }
 
